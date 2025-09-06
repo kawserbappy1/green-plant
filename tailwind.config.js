51fd358a30fb1bd7f34975c454e03ee18fe4fc03
kawser-hamid-bappy <bappydu2015@gmail.com>
2025-09-06 20:05:22 +0600

    Environment setup without node_modules

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        firstcolor: "#E10914",
+        secondColor: "#FAD035",
+        cardcolor_1: "#FFF3E1",
+        cardcolor_2: "#FFE4E9",
+        cardcolor_3: "#FFEDBA",
+        cardcolor_4: "#EAD6C0",
+      },
+      fontFamily: {
+        Inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
