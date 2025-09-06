dcad13297196efcfa2ad890756d0ed39d11a79cb
kawser-hamid-bappy <bappydu2015@gmail.com>
2025-09-07 02:28:00 +0600

    category menu added

--- src/main.js
+++ src/main.js
@@ -13,3 +13,33 @@ document.querySelectorAll(".nav-link ").forEach((link) => {
     menu.classList.toggle("left-[0]");
   });
 });
+
+// ***************** TREE RELATED JS CODE *****************
+// RESUED FUNCTION TO CATCH ID NAME FROM UI
+const getIdName = (id) => {
+  return document.getElementById(id);
+};
+
+// Load category menu
+const loadCategory = () => {
+  fetch("https://openapi.programming-hero.com/api/categories")
+    .then((res) => res.json())
+    .then((data) => showLoadedcategory(data.categories));
+};
+// show loades category
+const showLoadedcategory = (categories) => {
+  // console.log(categories);
+  getIdName("category-container").innerHTML = "";
+  categories.forEach((category) => {
+    console.log(category);
+    getIdName("category-container").innerHTML += `
+              <li
+                class="btn flex place-items-center cursor-pointer text-base text-title-color bg-transparent ease-in-out duration-500 hover:bg-[#15803D] hover:text-white justify-center lg:w-full lg:justify-start" >
+                ${category.category_name}
+              </li>
+    
+    
+    `;
+  });
+};
+loadCategory();
